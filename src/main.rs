2ecdd308081fa3aee886d83f27c5d7866005baf9
jack-chapman <jackchapman093@gmail.com>
2023-08-12 21:40:36 +0100

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use axum::{routing::get, Router};
+use axum::{extract::MatchedPath, http::Request, routing::get, Router};
+use tower_http::trace::TraceLayer;
+use tracing::info_span;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -6,7 +9,26 @@ async fn hello_world() -> &'static str {
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "idea-api=debug,tower_http=debug,axum::rejection=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    let matched_path = request.extensions().get::<MatchedPath>().map(MatchedPath::as_str);
+
+                    info_span!("http_request", method = ?request.method(), matched_path, test_field = tracing::field::Empty)
+                })
+        );
+
+    tracing::debug!("idea-api started");
 
     Ok(router.into())
 }
