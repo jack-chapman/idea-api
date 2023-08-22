c09d38c490b9c012e648390392348cb48d3dec9c
jack-chapman <jackchapman093@gmail.com>
2023-08-22 21:33:51 +0100

    Probably make code harder to understand

--- src/routes/auth/register.rs
+++ src/routes/auth/register.rs
@@ -3,27 +3,53 @@ use sqlx::PgPool;
 
 use crate::domain::auth::{signup_user, PasswordError, SignupUserError, SignupUserPayload};
 
+#[derive(serde::Serialize)]
+struct RegisterResponse {
+    message: String,
+}
+
+impl RegisterResponse {
+    fn with_message(message: &str) -> Self {
+        Self {
+            message: message.to_string(),
+        }
+    }
+}
+
+impl From<PasswordError> for RegisterResponse {
+    fn from(e: PasswordError) -> Self {
+        match e {
+            PasswordError::TooShort => RegisterResponse::with_message("password too short"),
+            PasswordError::DoesNotMatch => RegisterResponse::with_message("passwords must match"),
+        }
+    }
+}
+
+impl From<SignupUserError> for (StatusCode, Json<RegisterResponse>) {
+    fn from(e: SignupUserError) -> Self {
+        match e {
+            SignupUserError::UnknownError(e) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(RegisterResponse::with_message(&e)),
+            ),
+            SignupUserError::InvalidPassword(e) => {
+                (StatusCode::BAD_REQUEST, Json(RegisterResponse::from(e)))
+            }
+        }
+    }
+}
+
 #[tracing::instrument()]
 pub async fn register(
     State(pool): State<PgPool>,
     Json(payload): Json<SignupUserPayload>,
 ) -> impl IntoResponse {
     if let Err(e) = signup_user(&pool, payload).await {
-        match e {
-            SignupUserError::UnknownError(e) => {
-                return (StatusCode::INTERNAL_SERVER_ERROR, e);
-            }
-            SignupUserError::InvalidPassword(e) => {
-                return match e {
-                    PasswordError::TooShort => {
-                        (StatusCode::BAD_REQUEST, "password too short".to_string())
-                    }
-                    PasswordError::DoesNotMatch => {
-                        (StatusCode::BAD_REQUEST, "passwords must match".to_string())
-                    }
-                };
-            }
-        };
+        tracing::error!("Unable to register user");
+        return e.into();
     }
-    (StatusCode::OK, "user registered".to_string())
+    (
+        StatusCode::OK,
+        Json(RegisterResponse::with_message("user registered")),
+    )
 }
